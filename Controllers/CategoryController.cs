57c259777c678d9709bf13031b09b1c343012486
GlebMozglyakov <mozglgleb2004@gmail.com>
2023-01-11 11:05:45 +0500

    Добавьте файлы проекта.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objlist = _db.Category;
+            return View(objlist);
+        }
+
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
