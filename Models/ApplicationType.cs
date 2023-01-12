c498c3acd8a3a7aaf2604c810e7be9c5a7269257
GlebMozglyakov <mozglgleb2004@gmail.com>
2023-01-12 14:18:26 +0500

    Выполнил сам раб для секции 3

--- Models/ApplicationType.cs
+++ Models/ApplicationType.cs
@@ -1,9 +1,12 @@
-﻿namespace Rocky.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
 {
     public class ApplicationType
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
     }
 }
